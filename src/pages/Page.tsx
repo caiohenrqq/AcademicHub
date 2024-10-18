bc8ef41c99a5a890ee23e651186bc722a1347678
henrqq <caiohenrqq.profissional@gmail.com>
2024-10-18 13:18:19 -0400

    Adiciona inputs email e senha

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,17 +1,34 @@
-import { IonButtons, IonImg, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonIcon, IonInput, IonTextarea, IonItem, IonImg, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import usericon from "/usericon.svg";
+import passwordicon from "/passwordicon.svg";
 import './Page.css';
 
 const Page: React.FC = () => {
   return (
+
     <IonPage>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonImg
-            src="../public/favicon.png"
-            alt="AcademicHub, sua comunidade universitária :)"
-          ></IonImg>
-          <IonTitle>Academic<span className='title-hub'>Hub</span></IonTitle>
-        </IonHeader>
+      <IonContent className="no-scroll" fullscreen>
+        <div className="login-container">
+          <IonContent>
+            <IonImg
+              src="/favicon.png"
+              alt="AcademicHub, sua comunidade universitária :)"
+            ></IonImg>
+            <IonTitle>
+              Academic<span className="title-hub">Hub</span>
+            </IonTitle>
+
+            <IonItem className="input-item">
+              <IonIcon slot="start" icon={usericon} aria-hidden="true" />
+              <IonInput placeholder="Escreva seu e-mail"></IonInput>
+            </IonItem>
+
+            <IonItem className="input-item">
+              <IonIcon slot="start" icon={passwordicon} aria-hidden="true" />
+              <IonInput placeholder="Escreva sua senha" type="password"></IonInput>
+            </IonItem>
+          </IonContent>
+        </div>
       </IonContent>
     </IonPage>
   );

bfc2fe493d9b5d77b34baf4733dd689421872d56
henrqq <caiohenrqq.profissional@gmail.com>
2024-10-21 12:40:55 -0400

    Adiciona e centraliza âncora de recuperação de senha

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,11 +1,23 @@
-import { IonButton, IonIcon, IonInput, IonTextarea, IonItem, IonImg, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonButton,
+  IonIcon,
+  IonInput,
+  IonTextarea,
+  IonItem,
+  IonImg,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import usericon from "/usericon.svg";
 import passwordicon from "/passwordicon.svg";
-import './Page.css';
+import "./Page.css";
 
 const Page: React.FC = () => {
   return (
-
     <IonPage>
       <IonContent fullscreen>
         <section className="login-section">
@@ -18,16 +30,22 @@ const Page: React.FC = () => {
             Academic<span className="hub-letras">Hub</span>
           </IonTitle>
 
-          <div className='login-inputs'>
-            <IonItem >
+          <div className="login-inputs">
+            <IonItem>
               <IonIcon slot="start" icon={usericon} aria-hidden="true" />
               <IonInput placeholder="Escreva seu e-mail"></IonInput>
             </IonItem>
 
             <IonItem>
               <IonIcon slot="start" icon={passwordicon} aria-hidden="true" />
-              <IonInput placeholder="Escreva sua senha" type="password"></IonInput>
+              <IonInput
+                placeholder="Escreva sua senha"
+                type="password"
+              ></IonInput>
             </IonItem>
+
+            <a className="esqueceu-senha" href="#">Esqueceu sua senha?</a>
+
           </div>
         </section>
       </IonContent>
